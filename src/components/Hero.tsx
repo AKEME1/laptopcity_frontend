bd28767909cf09cf82d2ca6530ebe2dfd1cd91ae
Akeme <tedake36@gmail.com>
2024-10-29 04:10:47 -0700

    debounce is included

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,22 +2,46 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 import { filterByModel } from "../requestResponsehandler/filterProduct";
 import { useProductContext } from "../context/productContext";
+import { useEffect } from "react";
+import axios from "axios";
 const Hero = () => {
   const [model,setModel]=useState('') 
   const { setProductData,productData,isOpen,setIsOpen } = useProductContext(); 
 
 
 
-   const { data } = useQuery({
-    queryKey: ['filterBymodel',model],
-    queryFn: ()=>filterByModel(model),
-     enabled: !!model,
-     onSuccess: (data) => {
-    setProductData(data);  // Update the product data when the query succeeds
-  }
+  //  const { data } = useQuery({
+  //   queryKey: ['filterBymodel',model],
+  //   queryFn: ()=>filterByModel(model),
+  //    enabled: !!model,
+  //    onSuccess: (data) => {
+  //   setProductData(data);  // Update the product data when the query succeeds
+  // }
    
-  });
+  // });
   
+const handleFetchByModel= async(model:string)=>{
+const {data}= await axios.get(`http://localhost:8080/api/v1/products/partialSearch?model=${model}`)
+setProductData(data.data.partialDeta)
+console.log(data)
+
+}
+
+const handleGetAllProduct=async()=>{
+  const {data}=await axios.get( `http://localhost:8080/api/v1/products`)
+  setProductData(data.data.data)
+}
+
+useEffect(()=>{
+  const timer=setTimeout(()=>{
+    if(model.length===0){
+      return handleGetAllProduct()
+    }
+    handleFetchByModel(model)
+  },500)
+  return () => clearTimeout(timer);
+},[model])
+
   return (
     <div onClick={()=>setIsOpen(false)} className={`bg-gradient-to-r from-custom-green to-custom-blue border-t border-black w-full ${isOpen?'blur-sm':""}`}>
       <div className="max-w-[1200px] mx-auto h-[200px] flex justify-center items-center">
